e1582d1463f98bb58a92b47bf920558adaa13a07
glebfedorov911 <fedorov22134@mail.ru>
2023-07-12 18:27:37 +0300

    new files and commands

--- src/posts/router.py
+++ src/posts/router.py
@@ -3,13 +3,14 @@ import math
 
 from fastapi import APIRouter, Depends, HTTPException, Request, Form
 from fastapi.templating import Jinja2Templates
-from sqlalchemy import insert, select
+from sqlalchemy import insert, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth.models import User
 from src.database import get_async_session
 from src.posts.models import Post
 from src.app import current_active_user
+from src.posts.schemas import PostCreate, PostUpdate
 
 templates = Jinja2Templates(directory="src/templates")
 
@@ -21,7 +22,7 @@ router = APIRouter(
 @router.post("/addpost")
 async def add_post(
         user: User = Depends(current_active_user), session: AsyncSession = Depends(get_async_session),
-        head: str = Form(...), description: str = Form(...)
+        head: str = Form(...), description: str = Form(...), post: PostCreate = None
 ):
     try:
         data = {"head": head, "description": description, "user_id": user.id}
@@ -48,6 +49,7 @@ async def form_post(request: Request):
 @router.get("/feed-posts/{page}")
 async def posts(
         request: Request, session: AsyncSession = Depends(get_async_session), limit: int = 10, page: int = 1,
+        user: User = Depends(current_active_user)
 ):
     query = select(Post)
     result = await session.execute(query)
@@ -57,6 +59,40 @@ async def posts(
     pages = await session.execute(query)
     pages = json.dumps(math.ceil(len(pages.scalars().all())/10))
 
-    context = {"request":request, "posts":posts, "pages":pages}
+    context = {"request":request, "posts":posts, "pages":pages, "user": user.id}
 
     return templates.TemplateResponse("feed.html", context)
+
+@router.post("/edit-post/{id}")
+async def edit_post(
+        id:int, head: str = Form(...), description: str = Form(...), session: AsyncSession = Depends(get_async_session),
+        user: User = Depends(current_active_user)
+                    ):
+    try:
+        query = select(Post).where(Post.id == id)
+        result = await session.execute(query)
+
+        if not (result.scalars().all()[0].user_id == user.id):
+            raise Exception
+
+        data = {"head": head, "description": description, "user_id": user.id}
+        stmt = update(Post).values(**data).where(Post.id==id)
+        await session.execute(stmt)
+        await session.commit()
+
+        return {
+            "status": "success",
+            "detail": None,
+            "data": None
+        }
+
+    except Exception:
+        return HTTPException(status_code=500, detail={
+            "status": "not success",
+            "detail": None,
+            "data": None
+        })
+
+@router.get("/edit-form/{id}")
+async def edit_form(id: int, request: Request):
+    return templates.TemplateResponse("edit-post.html", {"request":request, "id":id})
