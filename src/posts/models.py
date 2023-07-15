95141a271e178e83b4069f3ff5e90ff79b4ae4dd
glebfedorov911 <fedorov22134@mail.ru>
2023-07-15 12:27:57 +0300

    new files and commands

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import relationship
+
 from src.database import Base
 from src.auth.models import User
 
@@ -13,8 +15,22 @@ class Post(Base):
     )
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    head = Column(String(length=255), nullable=False)
+    head = Column(String(length=255), nullable=False, unique=True)
     description = Column(String, nullable=False)
     created_at = Column(TIMESTAMP, default=datetime.utcnow)
     edit_at = Column(TIMESTAMP, default=datetime.utcnow)
     user_id = Column(UUID, nullable=False)
+
+    like = relationship("Like", backref="liketable")
+
+class Like(Base):
+    __tablename__ = "like"
+    __table_args__ = (
+            ForeignKeyConstraint(['user_id'], ['user.id']),
+            ForeignKeyConstraint(['post_id'], ['post.id']),
+            {"extend_existing": True},
+        )
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(UUID, nullable=False)
+    post_id = Column(Integer, nullable=False)
--- src/posts/router.py
+++ src/posts/router.py
@@ -3,12 +3,13 @@ import math
 
 from fastapi import APIRouter, Depends, HTTPException, Request, Form
 from fastapi.templating import Jinja2Templates
-from sqlalchemy import insert, select, update
+from sqlalchemy import insert, select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.operators import and_
 
 from src.auth.models import User
 from src.database import get_async_session
-from src.posts.models import Post
+from src.posts.models import Post, Like
 from src.app import current_active_user
 from src.posts.schemas import PostCreate, PostUpdate
 
@@ -51,22 +52,22 @@ async def posts(
         request: Request, session: AsyncSession = Depends(get_async_session), limit: int = 10, page: int = 1,
         user: User = Depends(current_active_user)
 ):
-    query = select(Post)
+    query = select(Post, Like).join(Like, isouter=True).distinct(Post.head)
     result = await session.execute(query)
 
-    posts = result.scalars().all()[::-1][(page-1)*10:][:limit]
+    posts = result.all()[::-1][(page-1)*10:][:limit]
 
     pages = await session.execute(query)
     pages = json.dumps(math.ceil(len(pages.scalars().all())/10))
 
-    context = {"request":request, "posts":posts, "pages":pages, "user": user.id}
+    context = {"request":request, "posts":posts, "pages":pages, "user": user.id, "like_posts":result.scalars().all()}
 
     return templates.TemplateResponse("feed.html", context)
 
 @router.post("/edit-post/{id}")
 async def edit_post(
         id:int, head: str = Form(...), description: str = Form(...), session: AsyncSession = Depends(get_async_session),
-        user: User = Depends(current_active_user)
+        user: User = Depends(current_active_user), post: PostUpdate = None
                     ):
     try:
         query = select(Post).where(Post.id == id)
@@ -94,5 +95,96 @@ async def edit_post(
         })
 
 @router.get("/edit-form/{id}")
-async def edit_form(id: int, request: Request):
-    return templates.TemplateResponse("edit-post.html", {"request":request, "id":id})
+async def edit_form(
+        id: int, request: Request, user: User = Depends(current_active_user),
+        session: AsyncSession = Depends(get_async_session)
+                    ):
+
+    query = select(Post).where(Post.id == id)
+    result = await session.execute(query)
+
+    context = {"request":request, "id":id, "user_id":user.id, "author":result.scalars().all()[0].user_id}
+
+    return templates.TemplateResponse("edit-post.html", context)
+
+@router.get("/delete/{id}")
+async def delete_post(
+        id: int, session: AsyncSession = Depends(get_async_session), user: User = Depends(current_active_user)
+):
+    try:
+        query = select(Post).where(Post.id == id)
+        result = await session.execute(query)
+
+        if user.id != result.scalars().all()[0].user_id:
+            raise Exception
+
+        stmt = delete(Post).where(Post.id==id)
+        await session.execute(stmt)
+        await session.commit()
+
+        return {
+            "status": "success",
+            "detail": None,
+            "data": None
+        }
+
+    except Exception:
+        return HTTPException(status_code=500, detail={
+            "status": "not success",
+            "detail": None,
+            "data": None
+        })
+
+@router.get("/addlike/{id}")
+async def like_post(
+        id: int, session: AsyncSession = Depends(get_async_session), user: User = Depends(current_active_user)
+):
+    try:
+
+        query = select(Post).where(Post.id==id)
+        result = await session.execute(query)
+
+        if result.scalars().all()[0].user_id == user.id:
+            raise Exception
+
+        data = {"user_id": user.id, "post_id": id}
+
+        stmt = insert(Like).values(**data)
+        await session.execute(stmt)
+        await session.commit()
+
+        return {
+            "status": "success",
+            "detail": None,
+            "data": None
+        }
+
+    except Exception:
+        return HTTPException(status_code=500, detail={
+            "status": "not success",
+            "detail": None,
+            "data": None
+        })
+
+@router.get("/deletelike/{id}")
+async def like_post(
+        id: int, session: AsyncSession = Depends(get_async_session), user: User = Depends(current_active_user)
+):
+    try:
+
+        stmt = delete(Like).where(and_(user.id == Like.user_id, Like.post_id == id))
+        await session.execute(stmt)
+        await session.commit()
+
+        return {
+            "status": "success",
+            "detail": None,
+            "data": None
+        }
+
+    except Exception:
+        return HTTPException(status_code=500, detail={
+            "status": "not success",
+            "detail": None,
+            "data": None
+        })
